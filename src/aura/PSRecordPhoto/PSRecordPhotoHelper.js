dfe49515b1854f35446fcf286e51ecc01d0bc434
Troy Hedges <thedges@salesforce.com>
2017-04-25 07:09:16 -0500

    Update design attribute approach

--- src/aura/PSRecordPhoto/PSRecordPhotoHelper.js
+++ src/aura/PSRecordPhoto/PSRecordPhotoHelper.js
@@ -5,19 +5,9 @@
 
         map['recordId'] = component.get('v.recordId');
         map['defaultURL'] = component.get('v.defaultURL');
+        map['source'] = component.get('v.source');
+        map['sourceValue'] = component.get('v.sourceValue');
 
-        if (typeof component.get('v.contentId') !== "undefined" && component.get('v.contentId').length > 0)
-        {
-        	map['contentId'] = component.get('v.contentId');
-        }
-        else if (typeof component.get('v.field') !== "undefined" && component.get('v.field').length > 0)
-        {
-        	map['field'] = component.get('v.field');
-        }
-        else if (typeof component.get('v.filename') !== "undefined" && component.get('v.filename').length > 0)
-        {
-        	map['filename'] = component.get('v.filename');
-        }
         console.log('paramMap=' + JSON.stringify(map));
 
         var action = component.get("c.getImageURL");
